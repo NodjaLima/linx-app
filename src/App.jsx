49ed39814f8d30cae0209f292f51cffd6db592e4
NodjaLima <nodjamplima@gmail.com>
2022-08-19 09:32:04 -0300

    primeiro commmit do projeto - Header  inserido

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/header/header'
+import SectionUm from './components/section-um/sectionUm'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Header />
+      <SectionUm />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/section-um/sectionUm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Informacoes from './informacoes'
+
+import './sectionUm.css'
+
+const SectionUm = () => {
+  return ( 
+    <div className='section-um-container'>
+      <Informacoes />
+    </div>
+   );
+}
+ 
+export default SectionUm;
